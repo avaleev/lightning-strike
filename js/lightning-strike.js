549aacfdf0b38341865b6237842dfebf895e046d
Anton Valeev <anton@valeev.biz>
2021-05-28 22:32:01 -0400

    Setting up canvas scene

--- js/lightning-strike.js
+++ js/lightning-strike.js
@@ -1,39 +1,51 @@
 'strict'
 
-class LightningStrike {
-  mainScene;
+class LightningOptions {
+  DEBUG;
   emitterField;
-  catcherField;
+  receiverField;
+}
 
-  DEBUG = false;
+class LightningStrike extends LightningOptions {
+  mainScene;
+  canvas;
+  resizeListener;
 
-  constructor(
-    mainScene,
-    emitterField,
-    catcherField,
-    options
-  ) {
+  constructor(mainScene, options) {
+    super();
     Object.seal(this);
 
     try {
-      if (!mainScene)
+      if (!mainScene || typeof mainScene !== 'object')
         throw new LightningStrikeError('Provided "Main Scene" DOM Element is invalid');
       this.mainScene = mainScene;
-  
-      if (!emitterField)
-        throw new LightningStrikeError('Provided "Emitter Field" DOM Element is invalid');
-      this.emitterField = emitterField;
-  
-      // verify that 
-      if (!catcherField)
-        throw new LightningStrikeError('Provided "Catcher Field" DOM Element is invalid');
-      this.catcherField = catcherField;
+
+      if (typeof options !== 'object')
+        throw new LightningStrikeError('Options must be an object, got '.concat(typeof options))
+      // read and assign options
+      this.processOptions(options);
+
+      // prepare canvas for drawing
+      this.prepareScene();
     } catch (ex) {
       console.error(ex);
     }
+  }
 
-    // process passed or defaulted options
-    this.processOptions(options);
+  prepareScene() {
+    // count existing scenes if any
+    const sceneCount = document.querySelectorAll("[id^='lightning-strike-scene-']").length
+    
+    // create canvas and give it a unique scene id
+    this.canvas = document.createElement('canvas');
+    this.canvas.setAttribute('id', 'lightning-strike-scene-' + sceneCount);
+    
+    // make sure it fills entirety of it's parent
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
+    this.canvas.style.zIndex = '0';
+
+    this.mainScene.appendChild(this.canvas);
   }
 
   processOptions(options) {
@@ -41,16 +53,16 @@ class LightningStrike {
       switch (key) {
         case 'DEBUG':
           if (typeof value !== 'boolean')
-            throw new LightningStrikeError('1st parameter expected value type - boolean, got '.concat(typeof value));
-
+            throw new LightningStrikeError('DEBUG parameter expected value type - boolean, got '.concat(typeof value));
           this.toggleDebug(value);
         default:
+          this[key] = value;
           break;
       }
     }
   }
 
-  toggleDebug(value = undefined) {
+  toggleDebug(value) {
     const DEBUG_CLASS_NAME = 'debug-enabled';
 
     if (this.mainScene.classList) {
@@ -66,6 +78,8 @@ class LightningStrike {
         this.mainScene.className = classes.join(' ');
       }
     }
+
+    this.DEBUG = value;
   }
 }
 
